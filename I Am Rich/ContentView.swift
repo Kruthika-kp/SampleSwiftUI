f11b393883aa36cd0d21266760f095d22ac859ee
Kruthika-kp <kruthikakp@gmail.com>
2021-08-06 21:06:24 +0530

    commit

---
+++ I Am Rich/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by Kruthika KP on 13/05/20.
+//  Copyright © 2020 Kruthika KP. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
